8dfd2e6b896ac90195751ae6d432ba04eac7bcd7
Jianlin Shi <jianlins@users.noreply.github.com>
2024-10-28 12:21:25 -0600

    check jdk17

--- MariaDB4p/check_jdk.py
+++ MariaDB4p/check_jdk.py
@@ -4,20 +4,41 @@ import subprocess
 import os
 from pathlib import Path
 from loguru import logger
-def is_jdk_installed():
+
+def get_jdk_version(executable='java'):
+    """
+    Returns the installed JDK version or None if JDK is not installed.
+    """
+    try:
+        result = subprocess.run([executable, '-version'], capture_output=True, text=True)
+        if result.returncode == 0:
+            version_line = result.stderr.split('\n')[0]  # Typically, version info is in stderr
+            version = int(version_line.split()[2].strip('"').split('.')[0])
+            logger.info(f"Installed JDK version: {version}")
+            return version
+        else:
+            logger.info("Failed to determine JDK version.")
+            return 0
+    except FileNotFoundError:
+        logger.info("JDK is not installed.")
+        return 0
+
+def is_jdk_installed(target_version=17):
     """
     Check if JDK is installed by attempting to run 'java -version'.
     """
     current_dir=Path(__file__).parent    
     logger.debug(f'current_dir:{current_dir}')
     if is_jdk_inpath():
-        return 'java'
+        if get_jdk_version()==target_version:
+            return 'java'
     elif 'JAVA_HOME'  in os.environ and len(os.environ['JAVA_HOME'])>0:
         logger.info(f"jdk has been installed to:{os.environ['JAVA_HOME']}")
         if not Path(current_dir, 'path.config').exists():
             java_exe=str(Path(os.environ['JAVA_HOME'],'bin','java'))
-            Path(current_dir, 'path.config').write_text(java_exe)
-        return str(Path(os.environ['JAVA_HOME'], 'bin','java'))
+            if get_jdk_version(java_exe)==target_version:
+                Path(current_dir, 'path.config').write_text(java_exe)
+                return str(Path(os.environ['JAVA_HOME'], 'bin','java'))
     elif Path(current_dir, 'path.config').exists():
         java_exe=Path(Path(current_dir,'path.config').read_text().strip())
         path_config=java_exe.parent.parent
@@ -26,7 +47,7 @@ def is_jdk_installed():
             os.environ['JAVA_HOME']=str(path_config)
             return java_exe
 
-    logger.info("JDK is not installed.")
+    logger.info(f"JDK{target_version} is not installed.")
     return False
     
 def is_jdk_inpath():
